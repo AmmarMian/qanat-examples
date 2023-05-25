f3cbfda9174a189fcde02747698113cbf6155464
Ammar Mian <ammar.mian@univ-smb.fr>
2023-05-25 15:34:32 +0200

    MC simulation basics

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,19 @@
 import numpy as np
 
 
+def matprint(mat, fmt="g"):
+    """ Pretty print a matrix in Python 3 with numpy.
+    Source: https://gist.github.com/lbn/836313e283f5d47d2e4e
+    """
+
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col])
+                 for col in mat.T]
+    for x in mat:
+        for i, y in enumerate(x):
+            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
+        print("")
+
+
 def format_covariance_latex(cov: np.ndarray) -> str:
     """Format 2D covariance matrix for LaTeX rendering.
 
